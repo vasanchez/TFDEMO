91c6a0720f254b57b2277a0f355193fbfe6ba73c
Victor Sanchez <v.a.sanchez@live.com>
2020-03-14 08:48:49 -0700

    check out branch

--- main.tf
+++ main.tf
@@ -10,3 +10,4 @@ resource "azurerm_resource_group" "resource_gp" {
 # gitignore was not working, had to clear cache, ran 2 commands below, include the period
 # git rm -r --force --cached .
 # git add .
+# testing 2
