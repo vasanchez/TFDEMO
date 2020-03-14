ad682b219b166d275086aba39a9873c9a5d634e4
Victor Sanchez <v.a.sanchez@live.com>
2020-03-14 11:19:19 -0700

    again

--- main.tf
+++ main.tf
@@ -1,8 +1,4 @@
 resource "azurerm_resource_group" "resource_gp" {
     name    = "TFdemo"
     location  = "eastus"
-}# gitignore was not working, had to clear cache, ran 2 commands below, include the period
-# git rm -r --force --cached .
-# git add .
-# testing 2
-# testing 5
+}
