2ce7b1f5193f50d67c448abdc17261f9037c9e69
Victor Sanchez <v.a.sanchez@live.com>
2020-03-14 11:04:25 -0700

    change made

--- main.tf
+++ main.tf
@@ -1,13 +1,7 @@
 resource "azurerm_resource_group" "resource_gp" {
     name    = "TFdemo"
     location  = "eastus"
-
-    tags {
-        owner = "Victor A. Sanchez"
-
-    }
-}
-# gitignore was not working, had to clear cache, ran 2 commands below, include the period
+}# gitignore was not working, had to clear cache, ran 2 commands below, include the period
 # git rm -r --force --cached .
 # git add .
 # testing 2
